9c9b658062850b03fc8c431d94e3fb3725589eb9
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-09-09 16:33:59 +0400

    changes

--- src/core/static-components/footer.tsx
+++ src/core/static-components/footer.tsx
@@ -1,11 +1,7 @@
 import { Card } from 'antd';
 
 function Footer() {
-  return (
-    <Card className="box text-center">
-      © «Optima Group CO» - 2023 Version 1.0
-    </Card>
-  );
+  return <Card className="box text-center">© E-doc » - 2023 Version 1.0</Card>;
 }
 
 export default Footer;
